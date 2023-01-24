ab80192f068688066e01962a104dddeaccc54d65
pratik <21pratikdhane@gmail.com>
2023-01-24 21:14:13 +0530

    deploy5

--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,19 @@ const path = require("path");
 const express = require("express");
 const colors = require("colors");
 const cors = require("cors");
-const dotenv = require("dotenv").config();
+const dotenv = require("dotenv");
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
-const port = process.env.PORT || 5000;
-
-connectDB();
 
 const app = express();
-app.use(cors());
 app.use(express.json());
+app.use(cors());
+const port = process.env.PORT || 5000;
+dotenv.config();
+connectDB();
+
+console.log(process.env.PORT);
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 app.use("/api/goals", require("./routes/goalRoutes"));

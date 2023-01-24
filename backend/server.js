c82f31020413780b69d0a91a3ff73e9581121da2
pratik <21pratikdhane@gmail.com>
2023-01-25 00:10:40 +0530

    deploy11

--- backend/server.js
+++ backend/server.js
@@ -13,9 +13,12 @@ const port = process.env.PORT || 5000;
 dotenv.config();
 connectDB();
 
-console.log(process.env.PORT);
+console.log(
+  process.env.PORT,
+  path.join(__dirname, "frontend/build", "index.html")
+);
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.use(express.static(path.join(__dirname, "frontend/build", "index.html")));
 app.use("/api/goals", require("./routes/goalRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/test", (req, res) => {

2676bdc028e9c92dd2d6a742e03f43e8d0800d67
pratik <21pratikdhane@gmail.com>
2023-01-24 20:25:27 +0530

    commmit

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const express = require("express");
 const colors = require("colors");
+const cors = require("cors");
 const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
@@ -9,7 +10,7 @@ const port = process.env.PORT || 5000;
 connectDB();
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
